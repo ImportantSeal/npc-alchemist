47d196c283cf07a79c9693dd61b3794a23faebba
Topi Vuori <topitvuori@gmail.com>
2025-03-12 12:23:06 +0200

    testroll length updated, ui for testing added

---
+++ src/components/testroll.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import names from '../data/names';
+
+const FirstNameRoller = () =>{
+
+    const [selectedName, setSelectedName] = useState('')
+
+    const rollName = () =>{
+        const races = Object.keys(names);
+        const randomRace = races[Math.floor(Math.random()* races.length)];
+        const nameList = names[randomRace];
+        const randomName = nameList[Math.floor(Math.random()* nameList.length)];
+        setSelectedName(`${randomName} (${randomRace})`);
+    };
+
+    return (
+        <div>
+            <h1>First Name Roller</h1>
+            <button onClick = {rollName}>Roll Name</button>
+            {selectedName && <p>{selectedName}</p>}
+        </div>
+    );
+};
+
+export default FirstNameRoller;
---
+++ src/data/names.js
@@ -0,0 +1,16 @@
+const names = {
+    Ihminen: [
+        'Mikael',
+        'Laura',
+        'Veikko',
+        'Sanna'
+    ],
+    Kääpiö: [
+        'SSSS',
+        'DDDD',
+        'NNNN',
+        'MMMM',
+    ]
+};
+
+export default names 
