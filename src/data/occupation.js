e4d940aa0bbd98d08d070335469b4b428f715fba
Topi Vuori <topitvuori@gmail.com>
2025-03-13 14:48:53 +0200

    completed jobs list

--- src/data/occupation.js
+++ src/data/occupation.js
@@ -1,17 +1,93 @@
 const occupation = {
-
     Käsityöläinen: [
         'Oluenpanija',
         'Muurari',
         'Puuseppä',
-        'Jalokivihioja'
+        'Jalokivihioja',
+        'Nahkatyöläinen',
+        'Lukkoseppä',
+        'Marmori veistäjä',
+        'Savenvalaja',
+        'Ompelija/ Räätäli',
+        'Laivanrakentaja',
+        'Seppä',
+        'Aseseppä',
+        'Kivenmuuraja',
+        'Nahkuri',
+        'Kutoja',
+        'Herbalisti'
     ],
-    Liikkeenharjoittaja:[
+    Liikkeenharjoittaja: [
         'Kopisti',
-        'Majatalonpitäjä, laadukas',
+        'Majatalonpitäjä, Laadukas',
+        'Majatalonpitäjä, Kaupunki',
+        'Majatalonpitäjä, Kylä',
+        'Koiratarhanpitäjä',
+        'Lääkäri',
         'Kirjuri',
         'Tallimestari'
+    ],
+    Muu: [
+        'Kokki',
+        'Minstreli',
+        'Satamamestari'
+    ],
+    Kauppias: [
+        'Kapakoitsija- Laadukas',
+        'Kapakoitsija- Kaupunki',
+        'Kapakoitsija- Kylä',
+        'Lihakauppias',
+        'Kalakauppias',
+        'Sekatavarakauppias'
+    ],
+    Palvelia: [
+        'Hovimestari',
+        'Kokki',
+        'Huutaja',
+        'Lakeija',
+        'Opas, Kaupunki',
+        'Opas, Luonto',
+        'Sanansaattaja',
+        'Keittiön apulainen',
+        'Sisäkkö',
+        'Viestinviejä, Luonto',
+        'Viestinviejä, Kaupunki',
+        'Soihdunkantaja',
+        'Kääntäjä'
+    ],
+    Sotilas: [
+        'Jousimies',
+        'Varsijousimies',
+        'Jalkamies, Raskas',
+        'Jalkamies, Kevyt',
+        'Metsästäjä',
+        'Palkkasoturi',
+        'Palkkasoturi, Ratsastaja',
+        'Pioneeri',
+        'Piiritys-insinööri',
+        'Sheriffi, Kaupunki',
+        'Sheriffi, Kylä',
+        'Vartija'
+    ],
+    Työläinen: [
+        'Leipuri',
+        'Kuljettaja, Vaunut',
+        'Viljelijä',
+        'Kalastaja',
+        'Metsänhoitaja',
+        'Puutarhuri',
+        'Tallirenki',
+        'Paimen',
+        'Työmies',
+        'Pyykkäri',
+        'Mylläri',
+        'Kaivosmies',
+        'Merimies'
+    ],
+    Yläluokka: [
+        'Ritari',
+        'Ritari, maataomistava'
     ]
 };
 
-export default occupation
+export default occupation;
